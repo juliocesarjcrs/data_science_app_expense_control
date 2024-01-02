6f6e8acb93dcbb0126b8afa334caad19a57fc72a
juliocesarjcrs <jcrico27@misena.edu.co>
2024-01-02 16:38:59 -0500

    save changes

--- src/training/model_training.py
+++ src/training/model_training.py
@@ -1,10 +1,25 @@
+from sklearn.model_selection import TimeSeriesSplit
+from statsmodels.tsa.arima.model import ARIMA
 class ModelTraining:
 
     def __init__(self, utils):
       self.utils = utils
       self.data = None
       self.data_without_outliers = None
-      self.datasets = None
+      self.datasets = []
+      self.model_metrics = []
+      self.tscv = TimeSeriesSplit(n_splits=5, test_size=6)
+
+      model_order = (0,1,1) # reemplazar con los valores obtenidos en el punto 2.1.2 y 2.1.3 p, d q
+
+      # ajustar el modelo
+      model_arima = ARIMA(df['cost'], order=model_order)
+
+      self.models = [
+        # ('Linear Regression', LinearRegression()),
+        # ('Random Forest', RandomForestRegressor())
+        ('Arima', model_arima)
+        ]
 
     def preprocess_data(self):
       file_name = "./data/processed/df_time_monthly.csv"
@@ -20,44 +35,53 @@ class ModelTraining:
           {'name': 'Without Outliers', 'train_ratio': 0.8, 'X': X_without_outliers, 'y': y_without_outliers}
           # Add more datasets if needed
       ]
+      from sklearn.model_selection import TimeSeriesSplit
+      from sklearn.metrics import mean_squared_error
+      import statsmodels.api as sm
+      tscv = TimeSeriesSplit(n_splits = 4)
+      rmse = []
+      for train_index, test_index in tscv.split(cross_validation):
+          cv_train, cv_test = cross_validation.iloc[train_index], cross_validation.iloc[test_index]
+          
+          arma = sm.tsa.ARMA(cv_train, (2,2)).fit(disp=False)
+          
+          predictions = arma.predict(cv_test.index.values[0], cv_test.index.values[-1])
+          true_values = cv_test.values
+          rmse.append(sqrt(mean_squared_error(true_values, predictions)))
+    
+print("RMSE: {}".format(np.mean(rmse)))
 
-    def train_and_evaluate(self):
+    def train_and_evaluate(self, use_cross_validation):
       for dataset in self.datasets:
           dataset_name = dataset['name']
           X = dataset['X'].values
           y = dataset['y'].values
-          for train_index, test_index in self.tscv.split(X):
-              train_X, test_X = X[train_index], X[test_index]
-              train_y, test_y = y[train_index], y[test_index]
-              test_X_df = pd.DataFrame(test_X, index=pd.to_datetime(dataset['X'].index[test_index]))
+          if use_cross_validation:
+            self.apply_cross_validations(dataset_name, X, y)
+          else:
+              # Código sin el bucle anidado
+              pass
 
-              model_arima_fit = self.fit_arima(train_y, train_X)
-              model_sarima_fit = self.fit_sarima(train_y, train_X, model_arima_fit.order, model_arima_fit.seasonal_order)
+    def apply_cross_validations(self, dataset_name, X, y):
+      for train_index, test_index in self.tscv.split(X):
+        train_X, test_X = X[train_index], X[test_index]
+        train_y, test_y = y[train_index], y[test_index]
 
-              for model_name, model in self.models:
-                  result, y_pred = self.evaluate_model(model_name, model, train_X, train_y, test_X, test_y)
-                  self.model_metrics.append(result)
-                  y_pred_series = pd.Series(y_pred, index=test_X_df.index)
-                  plot_time_series(pd.Series(train_y, index=pd.to_datetime(dataset['X'].index[train_index])),
-                                    pd.Series(test_y, index=pd.to_datetime(dataset['X'].index[test_index])),
-                                    y_pred_series,
-                                    f'Serie Tiempo - {dataset_name} - {model_name}')
-                  print('---')
+        for model_name, model in self.models:
+            result, y_pred = self.evaluate_model(model_name, model, train_X, train_y, test_X, test_y)
+            self.model_metrics.append(result)
+            y_pred_series = pd.Series(y_pred, index=test_X_df.index)
+            plot_time_series(pd.Series(train_y, index=pd.to_datetime(dataset['X'].index[train_index])),
+                              pd.Series(test_y, index=pd.to_datetime(dataset['X'].index[test_index])),
+                              y_pred_series,
+                              f'Serie Tiempo - {dataset_name} - {model_name}')
 
-              result_arima, y_pred_arima = self.evaluate_arima(model_arima_fit, train_y, test_y, test_X_df, dataset_name)
-              self.model_metrics.append(result_arima)
-              y_pred_arima_series = pd.Series(y_pred_arima, index=test_X_df.index)
-              plot_time_series(pd.Series(train_y, index=pd.to_datetime(dataset['X'].index[train_index])),
-                                pd.Series(test_y, index=pd.to_datetime(dataset['X'].index[test_index])),
-                                y_pred_arima_series,
-                                f'Serie Tiempo - {dataset_name} - Auto ARIMA')
-              print('---')
-
-              result_sarima, y_pred_sarima = self.evaluate_sarima(model_sarima_fit, train_y, test_y, test_X_df, dataset_name)
-              self.model_metrics.append(result_sarima)
-              y_pred_sarima_series = pd.Series(y_pred_sarima, index=test_X_df.index)
-              plot_time_series(pd.Series(train_y, index=pd.to_datetime(dataset['X'].index[train_index])),
-                                pd.Series(test_y, index=pd.to_datetime(dataset['X'].index[test_index])),
-                                y_pred_sarima_series,
-                                f'Serie Tiempo - {dataset_name} - SARIMA')
-              print('---')
+    def evaluate_model(self, model_name, model, train_X, train_y, test_X, test_y):
+      model.fit(train_X, train_y)
+      y_pred = model.predict(test_X)
+      metrics = evaluate_forecast(test_y, y_pred)
+      return {
+          'model_name': model_name,
+          'model_fit': model,
+          'metrics': metrics
+      }, y_pred
